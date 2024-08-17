a76766fc696cda4abac2e400b3a2b352537c7d20
nayeem-miah <nayeem5113a@gmail.com>
2024-08-17 17:22:56 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -18,13 +18,38 @@ app.use(express.json());
 
 
 
+console.log(process.env.DB_pass);
+
+const uri = `mongodb+srv://${process.envDB_user}:${process.env.DB_pass}@cluster0.bomlehy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+        const productCollection = client.db("productPlus").collection('product')
+       
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-    res.send('FilterShop-Backend')
+    res.send('productPlus-Backend')
 });
 
 app.listen(port, () => {
-    console.log(`FilterShop-Backend server is running on port: ${port}`);
+    console.log(`productPlus-Backend server is running on port: ${port}`);
 })
